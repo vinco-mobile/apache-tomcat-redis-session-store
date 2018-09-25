fe00052728313040b30909e5f92116c7bd14b7b7
tomas.couso <tomas.couso@gmail.com>
2018-09-25 12:38:24 +0200

    Se sube la documentacion y scripts

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ cp -rv ./docker/common ./docker/tomcat1/
 cp -rv ./docker/common ./docker/tomcat2/
 cp -rv ./docker/common ./docker/tomcat3/
 
+cd docker
 # Se para docker-compouse
 docker-compose down
 # Se construyen las imagenes
